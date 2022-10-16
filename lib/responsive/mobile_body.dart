42e980735b2ec5d62e3d4c0438177ce3e151c5c6
fortuna <avidia38@gmail.com>
2022-10-16 11:35:01 +0900

    feat: appBar and drawer in mobile_body

--- lib/responsive/mobile_body.dart
+++ lib/responsive/mobile_body.dart
@@ -6,7 +6,34 @@ class MobileScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      appBar: AppBar(
+        backgroundColor: Colors.grey[900],
+      ),
+      backgroundColor: Colors.grey[300],
+      drawer: Drawer(
+        backgroundColor: Colors.grey[300],
+        child: Column(
+          children: const [
+            DrawerHeader(child: Icon(Icons.favorite)),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('D A S H B O A R D'),
+            ),
+            ListTile(
+              leading: Icon(Icons.chat),
+              title: Text('M E S S A G E'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('S E T T I N G S'),
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('L O G O U T'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
